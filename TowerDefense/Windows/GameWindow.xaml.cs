7b60a3d10d9e7b5bba07170eb07f80eaa1eccb9c
kalavarda <kulerda.kalavarda@gmail.com>
2021-11-09 00:03:32 +0300

    2

--- TowerDefense/Windows/GameWindow.xaml.cs
+++ TowerDefense/Windows/GameWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using Kalavarda.Primitives.Visualization;
 using Frame = Kalavarda.Primitives.Visualization.Frame;
@@ -14,13 +15,22 @@ namespace TowerDefense.Windows
         {
             InitializeComponent();
 
+            _visualData = CreateTeapot();
+
+            _cbState.ItemsSource = _visualData.States.Select(st => st.Name).OrderBy(n => n);
+
+            Loaded += GameWindow_Loaded;
+        }
+
+        private static VisualData CreateCube()
+        {
             var idle = new View
             {
                 Angle = 321,
-                Frames = new []
+                Frames = new[]
                 {
-                    new Frame { RawData = GetRawData("Cube-0000.png") }
-                }, 
+                    new Frame {RawData = GetRawData("Cube-0000.png")}
+                },
                 DurationSec = 2.5f
             };
             var rotating = new View
@@ -28,39 +38,67 @@ namespace TowerDefense.Windows
                 Angle = 12,
                 Frames = new[]
                 {
-                    new Frame { RawData = GetRawData("Cube-0000.png") },
-                    new Frame { RawData = GetRawData("Cube-0010.png") },
-                    new Frame { RawData = GetRawData("Cube-0020.png") },
-                    new Frame { RawData = GetRawData("Cube-0030.png") },
-                    new Frame { RawData = GetRawData("Cube-0040.png") },
-                    new Frame { RawData = GetRawData("Cube-0050.png") },
-                    new Frame { RawData = GetRawData("Cube-0060.png") },
-                    new Frame { RawData = GetRawData("Cube-0070.png") },
-                    new Frame { RawData = GetRawData("Cube-0080.png") },
-                    new Frame { RawData = GetRawData("Cube-0090.png") }
+                    new Frame {RawData = GetRawData("Cube-0000.png")},
+                    new Frame {RawData = GetRawData("Cube-0010.png")},
+                    new Frame {RawData = GetRawData("Cube-0020.png")},
+                    new Frame {RawData = GetRawData("Cube-0030.png")},
+                    new Frame {RawData = GetRawData("Cube-0040.png")},
+                    new Frame {RawData = GetRawData("Cube-0050.png")},
+                    new Frame {RawData = GetRawData("Cube-0060.png")},
+                    new Frame {RawData = GetRawData("Cube-0070.png")},
+                    new Frame {RawData = GetRawData("Cube-0080.png")},
+                    new Frame {RawData = GetRawData("Cube-0090.png")}
                 },
                 DurationSec = 1f
             };
-            _visualData = new VisualData
+            return new VisualData
             {
-                States = new []
+                States = new[]
                 {
                     new State
                     {
                         Name = "Idle",
-                        Views = new [] { idle } 
+                        Views = new[] {idle}
                     },
                     new State
                     {
                         Name = "Active",
-                        Views = new [] { rotating }
+                        Views = new[] {rotating}
                     }
                 }
             };
+        }
 
-            _cbState.ItemsSource = _visualData.States.Select(st => st.Name).OrderBy(n => n);
+        private static VisualData CreateTeapot()
+        {
+            var views = new View[16];
+            for (var i = 0; i < views.Length; i++)
+            {
+                var fNo = (int)(100 * (float)i / views.Length);
+                var s = fNo.ToString("###");
+                while (s.Length < 3)
+                    s = "0" + s;
+                views[i] = new View
+                {
+                    Angle = (int) (360 * (float) i / views.Length),
+                    Frames = new[]
+                    {
+                        new Frame { RawData = GetRawData($"Teapot-0{s}.png") }
+                    }
+                };
+            }
 
-            Loaded += GameWindow_Loaded;
+            return new VisualData
+            {
+                States = new[]
+                {
+                    new State
+                    {
+                        Name = "Idle",
+                        Views = views
+                    }
+                }
+            };
         }
 
         private void GameWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
@@ -68,17 +106,10 @@ namespace TowerDefense.Windows
             _cubeVisualizer.VisualData = _visualData;
         }
 
-        private static byte[] GetRawData()
-        {
-            var data = new byte[123];
-            for (var i = 0; i < data.Length; i++)
-                data[i] = (byte)(i % 250);
-            return data;
-        }
-
         private static byte[] GetRawData(string fileName)
         {
-            using var file = new FileStream(Path.Combine(@"C:\_\11\08\CubeAnimation", fileName), FileMode.Open, FileAccess.Read, FileShare.Read);
+            var path = fileName.StartsWith("Teapot") ? @"C:\_\11\08\Teapot" : @"C:\_\11\08\CubeAnimation";
+            using var file = new FileStream(Path.Combine(path, fileName), FileMode.Open, FileAccess.Read, FileShare.Read);
             var data = new byte[file.Length];
             file.Read(data);
             return data;
@@ -92,6 +123,11 @@ namespace TowerDefense.Windows
             var name = (string)_cbState.SelectedItem;
             var state = _visualData.States.First(st => st.Name == name);
             _visualData.State = state;
+        }
+
+        private void OnSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            _visualData.Angle = (int)_slider.Value;
         }
     }
 }
